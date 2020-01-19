d0430e680ea96424c055757bafc8826444a2220d
eliasjay <zeliasgl@gmail.com>
2020-01-19 14:28:04 -0300

    adding web socket

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
+const morgan = require('morgan');
+const http = require('http');
+const { setupWebSocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb+srv://joseelias:joseelias@cluster0-4nmog.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
+
+app.use(morgan('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
